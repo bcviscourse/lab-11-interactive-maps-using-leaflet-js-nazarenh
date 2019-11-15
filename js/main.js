c33a609832ddc7143a267e348dcbcfba43c8ccb8
Helen Nazarenko <nazarenh@bc.edu>
2019-11-15 12:24:43 -0500

    edit

--- js/main.js
+++ js/main.js
@@ -7,21 +7,34 @@ let stationMap;
 // Start application by loading the data
 Promise.all([
 	fetch("https://gbfs.bluebikes.com/gbfs/en/station_information.json").then(r=>r.json()),
-	fetch("https://gbfs.bluebikes.com/gbfs/en/station_status.json").then(r=>r.json())
+	fetch("https://gbfs.bluebikes.com/gbfs/en/station_status.json").then(r=>r.json()),
+	fetch("data/MBTA-Lines.json").then(r=>r.json())
 ]).then(datasets=>{
 	let stationInfo = datasets[0];
 	let stationStatus = datasets[1];
-	let MBTAlines = {}; //datasets[2];
-
+	let MBTAlines = datasets[2];
+	
 	console.log(stationStatus);
 	
-	
+	document.querySelector('#last-updated').textContent = new Date(stationInfo.last_updated*1000).toString();
+
+	document.querySelector('#station-count').textContent = stationInfo.data.stations.length;
 	// data processing
-	
-	// convert array to map for easier access by station id
+		// convert array to map for easier access by station id
+	let statusMap = stationStatus.data.stations.reduce((acc, d, i)=>{
+		acc[d.station_id] = d;
+		return acc;
+	},{});
 
 	// combine station info with the station's status
-	let stations = [];
+	let stations = stationInfo.data.stations.map(d=>{
+		d = {// ES6 way to combine two objects
+			...d, 
+			...statusMap[d.station_id]
+		};
+		// console.log(d); // print d to see what data is available for each station
+		return d;
+	});
 
 	visualize(stations, MBTAlines);
 });
@@ -34,14 +47,23 @@ function visualize(stations, MBTAlines){
 	let map = L.map('station-map').setView(viewCenter, 13);
 
 
+
 	// Specify directory with leaflet images
-	// L.Icon.Default.imagePath = 'img';
+	L.Icon.Default.imagePath = 'img';
 
 	// Option 1: Load and display a tile layer on the map (OpenStreetMap)
-	// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  	// attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	// }).addTo(map);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 
+	
+	// L.geoJson: Represents a GeoJSON object or an array of GeoJSON objects. Allows you to parse GeoJSON data and display it on the map
+	// see how to specify the style from GeoJSON features: https://leafletjs.com/reference-1.5.0.html#geojson
+	L.geoJson(MBTAlines, {
+		style: feature=> ({color: feature.properties.LINE}), // inline function
+		weight: 8,
+		opacity: 0.8
+	}).addTo(map);
 
 	// Option 2: Load and display a tile layer on the map (Stamen)
 	// L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
@@ -53,31 +75,52 @@ function visualize(stations, MBTAlines){
 
 	// Option 3: Load and display a tile layer on the map (Carto)
 	// More info https://github.com/CartoDB/basemap-styles
-	L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png', {
-		attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	}).addTo(map);
+	// L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png', {
+	// 	attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	// }).addTo(map);
 
 	// Add an empty layer group for the markers
+	let allMarkers = L.layerGroup().addTo(map);
 
 	// 	Remove all layers before drawing markers
+	allMarkers.clearLayers();
 
-	// // Defining an icon class 
-
+	// // Defining an icon class : Represents an icon to provide when creating a marker.
+	let LeafIcon = L.Icon.extend({
+		options: {
+			shadowUrl: 'img/marker-shadow.png',
+			iconSize: [25, 41], // Size of the icon image in pixels.
+				iconAnchor: [12, 41], // The coordinates of the "tip" of the icon (relative to its top left corner)
+				popupAnchor: [0, -28] // The coordinates of the point from which popups will "open", relative to the icon anchor.
+		}
+	});
 
 	// // Instantiate icons
-
-
+	let redMarker = new LeafIcon({iconUrl:  'img/marker-red.png'});
+	let blueMarker = new LeafIcon({iconUrl:  'img/marker-blue.png'});
 	
 	
 
 
 	//  Create a marker for each station
 	stations.forEach(function(d){
-		// Add a marker for each station
+
+		// Set marker icon depending on the state of each station
+		let markerColor = (d.num_bikes_available == 0 || d.num_docks_available == 0) ? redMarker : blueMarker;
 
 		// Add a popup and bind it to the marker
-
+		// L.Marker is used to display clickable/draggable icons on the map.
+		let marker = L.marker([d.lat, d.lon], { icon: markerColor }) // Icon instance to use for rendering the marker.
+	
 		// Add a marker to the allMarkers layer
+		allMarkers.addLayer(marker);
+		
+		let popupContent = 	'<strong>' + d.name + '</strong><br/>';
+        popupContent +=	'Available Bikes: ' + d.num_bikes_available + '<br/>';
+        popupContent +=	'Available Docks: ' + d.num_docks_available;
+
+		marker.bindPopup(popupContent); // Binds a popup to the marker with the passed content 
+	
 	});
 
 	// Add a GeoJSON for MBTA lines
